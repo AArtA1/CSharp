995d5d866e8d861accd589ebd8cf2baf73ca1e1f
Artak Aslanyan <96864250+AArtA1@users.noreply.github.com>
2022-10-26 14:56:14 +0300

    Add files via upload

---
+++ Contest/Contest1/TaskB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int sum = 0;
+        bool pr = int.TryParse(Console.ReadLine(), out int a);
+        while(pr && a !=0)
+        {
+            sum += a % 2 != 0 ? a : 0;
+            pr = int.TryParse(Console.ReadLine(), out a);
+        }
+        if (!pr)
+        {
+            Console.WriteLine("Incorrect input");
+        }
+        else
+        {
+            Console.WriteLine(sum);
+        }
+    }
+}
